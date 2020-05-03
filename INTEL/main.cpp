45f2fe03656f39df6511abbc32df5a5546c91a3e
pktiuk <kotiuk@wp.pl>
2020-05-03 14:56:29 +0200

    Finish reading from file

--- INTEL/main.cpp
+++ INTEL/main.cpp
@@ -3,13 +3,18 @@
 #include <GL/glut.h> 
 #include "f.hpp"
 
+#define HEADER_BUFF_SIZE 26
+#define OFFSET_LOC 10
+#define X_LOC 18
+#define Y_LOC 22
 
-unsigned char *g_pBuffer=nullptr;
+char *g_pBuffer=nullptr;
 uint32_t X,Y;
-char header_buffer[26];
+char header_buffer[HEADER_BUFF_SIZE];
 
 void displayCallback()
 {
+    //f(g_pBuffer,X,Y);
     glDrawPixels(Y,X,GL_RGBA,GL_UNSIGNED_BYTE,g_pBuffer);
     glutSwapBuffers();
 }
@@ -29,19 +34,21 @@ int main(int argc, char *argv[])
 {
     std::ifstream input_image;
     input_image.open("in.bmp");
-    input_image.read(header_buffer,26);
+    input_image.read(header_buffer,HEADER_BUFF_SIZE);
+
     uint32_t size=read4ByteBuff(header_buffer+2);
     cout<<"Size: "<< size <<endl;
-    uint32_t offset=read4ByteBuff(header_buffer+10);
+    uint32_t offset=read4ByteBuff(header_buffer+OFFSET_LOC);
     cout<<"Offset: "<< offset<<endl;
-    X=read4ByteBuff(header_buffer+18);
+    X=read4ByteBuff(header_buffer+X_LOC);
     cout<<"X: "<< X<<endl;
-    Y=read4ByteBuff(header_buffer+22);
+    Y=read4ByteBuff(header_buffer+Y_LOC);
     cout<<"Y: "<< Y<<endl;
 
-    //X=10;
-    //Y=10;
-    //g_pBuffer = new unsigned char [X*Y*4];
+    g_pBuffer = new char [size-offset];
+    input_image.read(g_pBuffer,offset-HEADER_BUFF_SIZE);
+    input_image.read(g_pBuffer,size-offset);
+
     //glutInit(&argc,argv);
     //glutInitDisplayMode(GLUT_SINGLE);
     //glutInitWindowPosition(100,100);
