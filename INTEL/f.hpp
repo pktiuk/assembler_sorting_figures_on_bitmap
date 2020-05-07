2de65bbdbf3c62aabfff28c27c0b0a3bbe5f3e05
pktiuk <kotiuk@wp.pl>
2020-05-07 12:20:33 +0200

    Describe function f

--- INTEL/f.hpp
+++ INTEL/f.hpp
@@ -2,6 +2,14 @@
 #define F_HPP_
 
 extern "C"{
+    /**
+     * @brief function sorting figures on image from ste smallest to the biggest
+     * 
+     * @param tmp_buffer -buffer as big as pbuffer
+     * @param pbuffer -contains image
+     * @param x -width of image
+     * @param y -height of image
+     */
     void f(char *tmp_buffer,char *pbuffer, int x, int y);
 }
 
